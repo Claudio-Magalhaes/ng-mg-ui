c470160a2fc7f35838e9df5b387bd496f8fef058
Claudio magalhães <claudio.magalhaes@outlook.com.br>
2021-08-19 15:36:57 -0300

    excluindo código morto

--- projects/sidebar/src/public-api.ts
+++
@@ -1,7 +0,0 @@
-/*
- * Public API Surface of sidebar
- */
-
-export * from './lib/services/sidebar.service';
-export * from './lib/components/sidebar/sidebar.component';
-export * from './lib/sidebar.module';
